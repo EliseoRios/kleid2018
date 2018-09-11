532a5e3f0b41a6ee507c670ed4c45f9029b0b077
EliseoRios <eliseo.root@gmail.com>
2018-09-11 16:09:06 -0500

    Base

---
+++ routes/web.php
@@ -0,0 +1,84 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+Route::get('logout', 'Auth\LoginController@logout');
+Route::get('/home', function () {
+	return redirect('/');
+});
+
+Auth::routes();
+
+Route::group(['middleware' => ['auth']], function () {
+	Route::get('/', 'HomeController@index');
+
+	//Usuarios
+	Route::group(['prefix'=>'usuarios'], function(){
+	      Route::get('/','UsuariosController@index');
+	      Route::get('datatables','UsuariosController@datatables');
+	      Route::get('editar/{id}','UsuariosController@editar');
+	      Route::get('eliminar/{id}','UsuariosController@eliminar');
+	      Route::post('guardar','UsuariosController@guardar');
+	      Route::post('actualizar','UsuariosController@actualizar');
+	      Route::post('update_permisos','UsuariosController@update_permisos');      
+	     
+	});
+
+	//Clientes
+	/*Route::group(['prefix'=>'clientes'], function(){
+		Route::get('/', 'ClientesController@index');
+		Route::get('datatables', 'ClientesController@datatables');
+		Route::get('crear', 'ClientesController@crear');
+		Route::get('ver/{hash_id}', 'ClientesController@ver');
+		Route::get('editar/{hash_id}', 'ClientesController@editar');
+		Route::get('eliminar/{hash_id}', 'ClientesController@eliminar');
+		Route::get('abrir/{id}','ClientesController@abrir');
+		Route::get('contactos/{id}','ClientesController@contactos');
+		Route::get('oportunidades/{id}','ClientesController@oportunidades');
+		
+		Route::post('guardar', 'ClientesController@guardar');
+		Route::post('actualizar', 'ClientesController@actualizar');
+		Route::post('cerrar','ClientesController@cerrar');
+	});*/
+	
+	//Correos
+	/*Route::group(['prefix'=>'correos'], function(){
+		Route::get('/', 'CorreosController@index');
+		Route::get('datatable', 'CorreosController@datatable');
+		Route::get('crear', 'CorreosController@crear');			
+		Route::get('ver/{id}', 'CorreosController@ver');
+		Route::get('finalizar/{id}', 'CorreosController@finalizar');
+		Route::post('guardar', 'CorreosController@guardar');
+	});*/
+	
+    //Eventos
+	/*Route::group(['prefix' => 'eventos'],function(){
+        Route::get('/','EventosController@index');
+        Route::get('supervisar','EventosController@supervisar');
+        Route::get('datatables','EventosController@datatables');
+        Route::get('crear','EventosController@crear');
+        Route::get('editar/{id}','EventosController@editar');
+        Route::get('eliminar/{id}','EventosController@eliminar');
+        Route::get('calendario','EventosController@calendario');
+        Route::get('ver/{id}','EventosController@ver');
+        Route::post('imprimir','EventosController@imprimir');   
+        Route::post('guardar','EventosController@guardar');
+        Route::post('actualizar','EventosController@actualizar');
+        Route::post('concluir','EventosController@concluir');
+    	Route::group(['prefix' => 'notas'],function(){
+            Route::get('obtener_notas/{id}','EventosNotasController@obtener_notas');
+            //Route::get('eliminar/{id}','EventosNotasController@eliminar');
+            Route::post('guardar','EventosNotasController@guardar');
+            //Route::post('actualizar','EventosNotasController@actualizar');
+        });
+    });*/
+});
